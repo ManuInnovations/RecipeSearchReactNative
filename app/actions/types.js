b87daeaee57ebeb007f6f1d413c00fa103730541
Janie Tito <jtito@hotmail.com>
2017-07-09 19:20:24 +1200

    global state connected

--- app/actions/types.js
+++ app/actions/types.js
@@ -1,6 +1,6 @@
 export const SET_SEARCHED_RECIPES = 'SET_SEARCHED_RECIPES';
 
-
+export const ADD_RECIPE = 'ADD_RECIPE'
 
 
 //this is a constant that we use as an action in the application
--- app/reducers/recipes.js
+++ app/reducers/recipes.js
@@ -4,3 +4,15 @@ import * as types from '../actions/types';
 export const searchedRecipes = createReducer({}, {
 
 });
+
+export const recipeCount = createReducer(0, {
+  [types.ADD_RECIPE](state, action) {
+    return state + 1;
+  }
+});
+
+//no store as such, just a series of transformations of state. Listen for the change of ADD_RECIPE and then return the state or state + 10 etc...
+//
+//every time you return state you are returning a new object. Everything else is destroyed. You need to return a new state.
+//
+//
